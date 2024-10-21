35da2ea7e747bfd73b8e634660345caf3d522972
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2024-10-21 16:01:57 -0500

    server structure

---
+++ server/server.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port      string
+	JTWSecret string
+	DBUrl     string
+}
+type Server interface { // to satisfy the interface(for broker) must return a Config
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+// satisfy interface(broker)
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	switch { // check config values
+	case config.Port == "":
+		return nil, errors.New("Port is required")
+	case config.DBUrl == "":
+		return nil, errors.New("Database is required")
+	case config.JTWSecret == "":
+		return nil, errors.New("JWT is required")
+	}
+	// create server
+	return &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter() // handler
+	binder(b, b.router)
+	log.Println("Starting server on port:", b.config.Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatalln("Failed to start server:", err)
+	}
+}
